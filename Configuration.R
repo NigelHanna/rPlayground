6540d5d333d58de72d0a3ddc33f9646404e019c5
Nigel Hanna <Nigel_Hanna@Yahoo.com>
2023-10-21 22:15:28 +1100

    Downloaded and set up the tidyverse

--- Configuration.R
+++ Configuration.R
@@ -1,2 +1,17 @@
 ### Install and load the tidyverse
 
+### Install from CRAN
+### Instructions from https://tidyverse.tidyverse.org/
+
+install.packages("tidyverse")
+
+### Load the tidyverse packages
+
+library(tidyverse)
+
+
+### Check conflicts
+tidyverse_conflicts()
+
+### Check that all packages are up to date
+tidyverse_update()
